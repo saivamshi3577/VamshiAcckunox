48ecc0edcb176cac22e9a586e1824a8e925b15e2
saivamshi3577 <saivamshichary2000@gmail.com>
2024-08-24 15:34:15 +0530

    changed

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import Chart from "chart.js"
+import Chart from "Chart.js";
 import './Category.css';
 
 const Category = ({ category, onAddWidget, onRemoveWidget }) => {
